7f1367bef28ed35d3407c71bf25b6ca8611f3e66
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-15 15:27:37 +0700

    Add files via upload

---
+++ luas lingkaran.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	
+	int r,luas;
+	cout <<"Masukkan Jari-Jari = ";
+	cin >>r;
+	
+	luas = 3.14 * r * r;
+	cout<<"Luas Lingkaran = " << luas;
+	
+	return 0;
+}
